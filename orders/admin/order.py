a6cf89779293eb022239fd14d5a20bcfcb42df6c
OstapSavyskyi <ostap.v.savytskyi@ukd.edu.ua>
2024-11-25 19:30:59 +0200

    Models Apps Fix_2

--- orders/admin/order.py
+++ orders/admin/order.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order
+from orders.models import Order
 
 # Register your models here.
 
--- recommendations/admin/recommendation.py
+++ recommendations/admin/recommendation.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recommendation
+from recommendations.models import Recommendation
 # Register your models here.
 
 
