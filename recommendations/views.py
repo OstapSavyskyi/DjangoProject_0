a4849b6953f8ccb83d467b27a9da22aee320a461
OstapSavyskyi <ostap.v.savytskyi@ukd.edu.ua>
2024-11-25 23:02:40 +0200

    Create views

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Recommendation
+from .serializers import RecommendationSerializer
 # Create your views here.
+
+
+class RecommendationViewSet(ModelViewSet):
+    queryset = Recommendation.objects.all()
+    serializer_class = RecommendationSerializer
